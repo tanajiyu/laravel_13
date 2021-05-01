410a5e1bc9f1f92755f9d0566a778981c56ec247
aaz <itdev04.ho@lippoinsurance.com>
2021-05-01 11:27:27 +0700

    edit model department

--- app/Department.php
+++ app/Department.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
+    protected $fillable = ['name'];
+    
     public function position(){
         return $this->hashMany('App\Position');
     } 
